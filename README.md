2efa607f9858c9a1625b4496b79d98d2fecb5e4b
Bruce Gordon <brucegne@gmail.com>
2020-03-17 19:13:39 -0400

    turd

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # FamilyDirectory
 List of the Gordon Family of Nebraska
+
+keytool -genkey -v -keystore family.keystore -alias family -keyalg RSA -keysize 2048 -validity 10000
+
---
+++ index.js
@@ -0,0 +1,16 @@
+
+<script>
+
+  const firebaseConfig = {
+    apiKey: "AIzaSyBFTDAUZWhpPkKGGiE1DvILAlI0yV-msNw",
+    authDomain: "family-863a0.firebaseapp.com",
+    databaseURL: "https://family-863a0.firebaseio.com",
+    projectId: "family-863a0",
+    storageBucket: "family-863a0.appspot.com",
+    messagingSenderId: "692970311427",
+    appId: "1:692970311427:web:cf095ed0201c76e17d2336"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+</script>
